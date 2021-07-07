5ec38e5e93b7354df1210e2000960cd4d055c9a6
Rodrigo S Almeida <almeida.rodrigosantana@gmail.com>
2021-07-07 16:22:43 -0300

    Finalizada feature.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/views/list-tarefas/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'list-tarefas'},
+  {path: 'list-tarefas', component: ListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
