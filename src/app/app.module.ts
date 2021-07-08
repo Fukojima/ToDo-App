c16f580868ae1f0656726116e4a9f3d0303fb642
Rodrigo S Almeida <almeida.rodrigosantana@gmail.com>
2021-07-08 13:00:08 -0300

    Ajustes pos merge defeituosa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -25,7 +24,7 @@ import { CardHomeComponent } from './components/views/card-home/card-home.compon
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    ListComponent
+    ListComponent,
     HomeComponent,
     CardHomeComponent
   ],
@@ -35,9 +34,6 @@ import { CardHomeComponent } from './components/views/card-home/card-home.compon
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule ,
-    MatCardModule,
-    MatListModule,
-    HttpClientModule
     MatCardModule,
     MatListModule,
     MatBottomSheetModule,
