095967f96e038b94f3816b1e817938836da90846
Souza <leonardo.souza@capgemini.com>
2021-07-08 08:59:10 -0300

    dev

--- src/app/components/views/create/create.component.ts
+++ src/app/components/views/create/create.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { RestApiService } from './../../../shared/rest-api.service';
+import { Todo } from '../../../model/todo';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.scss']
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public todoDetail = <Todo>{};
+  constructor( public restApi:RestApiService, public router:Router ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+   
+  }
+  
+
+  
+  submit(form : any) {
+    console.log(form);
+    if(form.valid) {
+        this.restApi.createTarefa(this.todoDetail);
+        this.router.navigate(['/list']);
+    } else {
+    
+    }
+  }
+  onClickCancel() {
+    this.router.navigate(['/todo-list']);
   }
 
 }
